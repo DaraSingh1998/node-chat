0bb953dc33693105f1004ea29d8d3ac7a8f32da7
Vibhor Bakshi <vibhor.bakshi1998@gmail.com>
2018-08-16 19:30:57 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const publicPath=path.join(__dirname,'../public');
 
-var port=process.env.PORT||3000;
+var port=process.env.PORT ||3000;
 var app=express();
 app.use(express.static(publicPath));
 app.listen(port,()=>{
