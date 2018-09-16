e22b910ed6ca35dfedf42911907451cb6dd81d41
Vibhor Bakshi <vibhor.bakshi1998@gmail.com>
2018-09-16 11:16:29 +0530

    Added Timestamp in app using moments

---
+++ playg/time.js
@@ -0,0 +1,9 @@
+var moment=require('moment');
+
+// Date
+var date=moment();
+console.log(date.format('MMM, Do, YYYY'));
+
+// Time
+var time=moment();
+console.log(time.format('hh:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,17 @@ socket.on('connect',function(){
 });
 
 socket.on('newMessage',function(msg){
-  console.log('New Message',msg);
+  var formattedTime=moment(msg.createdAt).format('h:mm a');
   var li=jQuery('<li></li>');
-  li.text(`${msg.from}: ${msg.text}`);
+  li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage',function (msg){
+  var formattedTime=moment(msg.createdAt).format('h:mm a');
   var li=jQuery('<li></li>');
   var a=jQuery('<a target="_blank">My current location</a>');
-  li.text(`${msg.from}: `);
+  li.text(`${msg.from} ${formattedTime}: `);
   a.attr('href',msg.url);
   li.append(a);
   jQuery('#messages').append(li);
